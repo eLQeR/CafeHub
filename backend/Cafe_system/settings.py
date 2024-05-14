53db50137728cf027eea424b97feacdeaa3de3d4
Kyrylo <kbuchinskiy1@gmail.com>
2024-05-14 21:47:13 +0300

    fix settings

--- backend/Cafe_system/settings.py
+++ backend/Cafe_system/settings.py
@@ -167,4 +167,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cafehubtest@gmail.com'
-EMAIL_HOST_PASSWORD = 'buchinskiy.kirilo'
+EMAIL_HOST_PASSWORD = '???'
