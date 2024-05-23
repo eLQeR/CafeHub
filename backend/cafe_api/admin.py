3db20b0d7ff0e967e6344947dcee8bd7626b7a19
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-05-24 01:30:15 +0300

    Add inline review image to review model in admin panel

--- backend/cafe_api/admin.py
+++ backend/cafe_api/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Cafe, Contact, Feature, Gallery, Review
+from cafe_api.models import Cafe, Contact, Feature, Gallery, Review, Metro, LineOfMetro, ReviewImage
 
-models = [Cafe, Contact, Feature, Gallery, Review]
+models = [Cafe, Contact, Feature, Gallery, Metro, LineOfMetro]
 for model in models:
     admin.site.register(model)
+
+class ReviewImageInline(admin.TabularInline):
+    fk_name = 'review'
+    model = ReviewImage
+    extra = 5
+
+@admin.register(Review)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ["article"]
+    inlines = [ReviewImageInline]
