2b04d9410b2f0efa242e813db52a5d2f5c813890
foxstudent103376 <yashopua@gmail.com>
2024-07-07 16:37:22 +0300

    add error indication on search fields

--- frontend/src/components/SearchField/SearchField.tsx
+++ frontend/src/components/SearchField/SearchField.tsx
@@ -3,15 +3,30 @@
 import React, { useState } from 'react';
 import s from './SearchField.module.scss';
 import { useRouter } from 'next/navigation';
+import cn from 'classnames';
 
 export const SearchField = ({ style }: { style: string }) => {
-  console.log(style);
   const [searchValue, setSearchValue] = useState('');
+  const [err, setErr] = useState(false);
+
   const router = useRouter();
 
   const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    setErr(false);
     if (e.key === 'Enter' && searchValue !== '') {
       router.push(`/search?name=${searchValue}`);
+    } else if (searchValue === '' && e.key === 'Enter') {
+      setErr(true);
+    }
+  };
+
+  const btnHandler = () => {
+    setErr(false);
+    if (searchValue !== '') {
+      router.push(`/search?name=${searchValue}`);
+    }
+    if (searchValue === '') {
+      setErr(true);
     }
   };
 
@@ -20,14 +35,20 @@ export const SearchField = ({ style }: { style: string }) => {
       <input
         type='text'
         placeholder='Пошук...'
-        className={s[style]}
+        className={cn(s[style], {
+          [s[style+'__war']]: err,
+        })}
         value={searchValue}
         onKeyDown={(e) => keyDownHandler(e)}
         onChange={(e) => {
           setSearchValue(e.target.value);
         }}
       />
-      {style === 'hero' && <button className={s.hero__btn}>Пошук</button>}
+      {style === 'hero' && (
+        <button className={s.hero__btn} onClick={btnHandler}>
+          Пошук
+        </button>
+      )}
     </>
   );
 };
